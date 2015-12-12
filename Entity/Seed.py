04c4b8b89866644d9db88e6807f7586f957ac535
mauribesson <mauribesson@gmail.com>
2015-12-12 14:14:02 -0300

    Actualizacion del repositorio

---
+++ Entity/Seed.py
@@ -0,0 +1,63 @@
+class Seed:
+    def __init__(self):
+        self.__type = ""
+        self.__name = ""
+        self.__time = ""
+        self.__earth = ""
+        self.__water = ""
+        self.__nutrients = ""
+        self.__chemicalFertilizer = ""
+
+        """
+         Getter y Setter
+        """
+    def get_type(self):
+        return self.__type
+
+    def set_type(self, type):
+        self.__type = type
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_time(self):
+        return self.__time
+
+    def set_time(self, time):
+        self.__time = time
+
+    def get_earth(self):
+        return self.__earth
+
+    def set_earth(self, earth):
+        self.__earth = earth
+
+    def get_water(self):
+        return self.__water
+
+    def set_water(self, water):
+        self.__water = water
+
+    def get_nutrients(self):
+        return self.__nutrients
+
+    def set_nutrients(self, nutrients):
+        self.__nutrients = nutrients
+
+    def get_chemical_fertilizer(self):
+        return self.__chemicalFertilizer
+
+    def set_chemical_fertilizer(self, chemical_fertilizer):
+        self.__chemicalFertilizer = chemical_fertilizer
+
+pass
+#Fin de la clase Seed.
+
+
+
+
+
+

dcfc1047a4bbc5b420212125f56e4d3817c51437
mauribesson <mauribesson@gmail.com>
2016-01-06 23:01:12 -0300

    commit

--- Control/ControlTimer.py
+++ Control/ControlTimer.py
@@ -1,3 +1,11 @@
 class ControlTimer:
     def __init__(self):
         self.listTime =[]
+
+    #controla el bucle de riempo
+    def chronos(self):
+        while 1 == 1:
+            print("time now:")
+
+
+
